ce0078900bb538c164a8cc37fe6c7ddc840c8616
aungkokomin <aungkokomin.webdeveloper@gmail.com>
2025-09-10 18:28:51 +0800

    Add basic Flask application with routing and views

---
+++ flaskDev/2basicRoutingAndViews.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+@app.route('/about')
+def about():
+    return "This is the about page. Our application is built using Flask."
+
+@app.route('/user/<username>')
+def show_user(username):
+    return f"This is the profile page of {username}."
+
+if __name__ == "__main__":
+    app.run(debug=True)
